63e2508ca1f9a9a898f818663a5e65e8a04f3d43
infinex-exchange <admin@infinex.cc>
2023-10-01 14:33:47 +0200

    Refactor 3

--- src/API/AssetsAPI.php
+++ src/API/AssetsAPI.php
@@ -1,10 +1,9 @@
 <?php
 
-require_once __DIR__.'/validate.php';
-
 use Infinex\Exceptions\Error;
+use Infinex\Pagination;
 
-class PasswordAPI {
+class AssetsAPI {
     private $log;
     private $amqp;
     private $pdo;
@@ -14,195 +13,52 @@ class PasswordAPI {
         $this -> amqp = $amqp;
         $this -> pdo = $pdo;
         
-        $this -> log -> debug('Initialized password API');
+        $this -> log -> debug('Initialized assets API');
     }
     
     public function initRoutes($rc) {
-        $rc -> put('/password', [$this, 'changePassword']);
-        $rc -> delete('/password', [$this, 'resetPassword']);
-        $rc -> patch('/password', [$this, 'confirmResetPassword']);
+        $rc -> get('/assets', [$this, 'getAllAssets']);
     }
     
-    public function changePassword($path, $query, $body, $auth) {
-        if(!$auth)
-            throw new Error('UNAUTHORIZED', 'Unauthorized', 401);
+    public function getAllAssets($path, $query, $body, $auth) {
+        $pag = new Pagination\Offset(50, 500, $query);
         
-        if(!isset($body['oldPassword']))
-            throw new Error('MISSING_DATA', 'oldPassword', 400);
-        if(!isset($body['password']))
-            throw new Error('MISSING_DATA', 'password', 400);
+        $task = [];
         
-        if(!validatePassword($body['oldPassword']))
-            throw new Error('VALIDATION_ERROR', 'old_password', 400);
-        if(!validatePassword($body['password']))
-            throw new Error('VALIDATION_ERROR', 'password', 400);
+        $sql = "SELECT sid,
+                       wa_remember,
+                       EXTRACT(epoch FROM wa_lastact) AS wa_lastact,
+                       wa_browser,
+                       wa_os,
+                       wa_device
+               FROM sessions
+               WHERE uid = :uid
+               AND origin = 'WEBAPP'
+               ORDER BY sid DESC"
+             . $pag -> sql();
         
-        $task = array(
-            ':uid' => $auth['uid']
-        );
-    
-        $sql = 'SELECT password
-                FROM users
-                WHERE uid = :uid';
-    
         $q = $this -> pdo -> prepare($sql);
         $q -> execute($task);
-        $row = $q -> fetch();
-    
-        if(! $row || !password_verify($body['oldPassword'], $row['password']))
-            throw new Error('INVALID_PASSWORD', 'Incorrect old password', 401);
+        
+        $sessions = [];
+        
+        while($row = $q -> fetch()) {
+            if($pag -> iter()) break;
             
-        if($body['oldPassword'] == $body['password'])
-            return;
-    
-        $hashedPassword = password_hash($body['password'], PASSWORD_DEFAULT);
-    
-        $task = array(
-            ':uid' => $auth['uid'],
-            ':password' => $hashedPassword
-        );
-    
-        $sql = 'UPDATE users
-                SET password = :password
-                WHERE uid = :uid';
-    
-        $q = $this -> pdo -> prepare($sql);
-        $q -> execute($task);
-    }
-    
-    public function resetPassword($path, $query, $body, $auth) {
-        if($auth)
-            throw new Error('ALREADY_LOGGED_IN', 'Already logged in', 403);
-        
-        if(!isset($body['email']))
-            throw new Error('MISSING_DATA', 'email', 400);
-        
-        if(!validateEmail($body['email']))
-            throw new Error('VALIDATION_ERROR', 'email', 400);
-        
-        $email = strtolower($body['email']);
-        
-        $task = array(
-            ':email' => $email
-        );
-        
-        $sql = 'SELECT uid
-                FROM users
-                WHERE email = :email';
-        
-        $q = $this -> pdo -> prepare($sql);
-        $q -> execute($task);
-        $row = $q -> fetch();
-        
-        if(! $row)
-            return;
-        
-        $uid = $row['uid'];
-        $generatedCode = sprintf('%06d', rand(0, 999999));
-    
-        $task = array(
-            ':uid' => $uid,
-            ':code' => $generatedCode
-        );
-        
-        $sql = "INSERT INTO email_codes (
-            uid,
-            context,
-            code
-        )
-        VALUES (
-            :uid,
-            'PASSWORD_RESET',
-            :code
-        )";
-    
-        $q = $this -> pdo -> prepare($sql);
-        $q -> execute($task);
-        
-        $this -> amqp -> pub(
-            'mail',
-            [
-                'uid' => $uid,
-                'template' => 'password_reset',
-                'context' => [
-                    'code' => $generatedCode
-                ],
-                'email' => $email
-            ]
-        );
-    }
-    
-    public function confirmResetPassword($path, $query, $body, $auth) {
-        if($auth)
-            throw new Error('ALREADY_LOGGED_IN', 'Already logged in', 403);
-        
-        if(!isset($body['email']))
-            throw new Error('MISSING_DATA', 'email', 400);
-        if(!isset($body['code']))
-            throw new Error('MISSING_DATA', 'code', 400);
-        if(!isset($body['password']))
-            throw new Error('MISSING_DATA', 'password', 400);
-        
-        if(!validateEmail($body['email']))
-            throw new Error('VALIDATION_ERROR', 'email', 400);
-        if(!validateVeriCode($body['code']))
-            throw new Error('VALIDATION_ERROR', 'code', 400);
-        if(!validatePassword($body['password']))
-            throw new Error('VALIDATION_ERROR', 'password', 400);
-        
-        $email = strtolower($body['email']);
-        
-        $this -> pdo -> beginTransaction();
-    
-        $task = array(
-            ':email' => $email,
-            ':code' => $body['code']
-        );
-        
-        $sql = "SELECT email_codes.codeid,
-                users.uid
-            FROM email_codes,
-                users
-            WHERE email_codes.code = :code
-            AND email_codes.context = 'PASSWORD_RESET'
-            AND users.email = :email
-            AND email_codes.uid = users.uid
-            FOR UPDATE OF email_codes";
-        
-        $q = $this -> pdo -> prepare($sql);
-        $q -> execute($task);
-        $row = $q -> fetch();
-        
-        if(! $row) {
-            $this -> pdo -> rollBack();
-            throw new Error('INVALID_VERIFICATION_CODE', 'Invalid verification code', 401);
+            $sessions[] = [
+                'sid' => $row['sid'],
+                'lastAct' => $row['wa_lastact'] ? intval($row['wa_lastact']) : null,
+                'browser' => $row['wa_browser'],
+                'os' => $row['wa_os'],
+                'device' => $row['wa_device'],
+                'current' => ($auth['sid'] == $row['sid']) 
+            ];
         }
-        $uid = $row['uid'];
         
-        $task = array(
-            ':codeid' => $row['codeid']
-        );
-        
-        $sql = 'DELETE FROM email_codes WHERE codeid = :codeid';
-        
-        $q = $this -> pdo -> prepare($sql);
-        $q -> execute($task);
-        
-        $hashedPassword = password_hash($body['password'], PASSWORD_DEFAULT);
-        
-        $task = array(
-            ':uid' => $uid,
-            ':password' => $hashedPassword
-        );
-        
-        $sql = 'UPDATE users
-            SET password = :password
-            WHERE uid = :uid';
-        
-        $q = $this -> pdo -> prepare($sql);
-        $q -> execute($task);
-        
-        $this -> pdo -> commit();
+        return [
+            'sessions' => $sessions,
+            'more' => $pag -> more
+        ];
     }
 }
 
